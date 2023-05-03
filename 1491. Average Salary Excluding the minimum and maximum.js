4392921215cd573ce250793c0c98a648451f3cfe
CerenGKB <cerengokbulut@hotmail.com>
2023-05-03 13:33:04 +0300

    Added comments

--- 1491. Average Salary Excluding the minimum and maximum.js
+++ 1491. Average Salary Excluding the minimum and maximum.js
@@ -1,8 +1,15 @@
+/* 
+I thought since this question wants to exclude minimum and maximum i can just find them with Math.min 
+and Math.max then i can use reduce method to add everything together,extract min-max value and find
+the solution by dividing sum to length-2 
+
+ */
+
 /**
  * @param {number[]} salary
  * @return {number}
  */
- 
+
 var average = function(salary) {
   let min = Math.min(...salary)
   let max = Math.max(...salary)
