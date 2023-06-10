9662f1ab1a28015f47f0edc1d8e2fc9f7d531562
CerenGKB <cerengokbulut@hotmail.com>
2023-06-10 19:51:42 +0300

    Fibonacci Sequence

---
+++ Javascript 30 day Challenge/2648.Generate Fibonacci Sequence.js
@@ -0,0 +1,5 @@
+//Day 29 - Javascript 30 day challenge
+
+/* Write a generator function that returns a generator object which yields the fibonacci sequence.
+The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13. */
