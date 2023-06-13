3506bbcf0442dfbf40c33a4ef0f86ccc58162455
CerenGKB <cerengokbulut@hotmail.com>
2023-06-13 19:41:12 +0300

    Object instance of a class

---
+++ Javascript 30 day Challenge/2618.Check if Object Instance of Class.js
@@ -0,0 +1,9 @@
+// Day 25- Javascript 30 day challenge
+
+
+/* 
+Write a function that checks if a given value is an instance of a given class or superclass. 
+For this problem, an object is considered an instance of a given class if that object has access to that class's methods.
+There are no constraints on the data types that can be passed to the function. For example, the value or the class could be undefined.
+
+ */
